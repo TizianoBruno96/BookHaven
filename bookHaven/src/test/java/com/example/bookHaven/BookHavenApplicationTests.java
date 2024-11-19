e6e2c15da17e458d6f77013df3ba92865545a36a
Tiziano Bruno <brntiziano96@gmail.com>
2024-11-19 23:08:24 +0100

    First Commit

---
+++ bookHaven/src/test/java/com/example/bookHaven/BookHavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookHaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
