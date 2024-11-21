94491466ef993fc7e728c75f0aea750c8028b8cd
Tiziano Bruno <brntiziano96@gmail.com>
2024-11-21 15:56:23 +0100

    service methods added

--- backend/src/main/java/com/example/bookHaven/service/INotificationService.java
+++ backend/src/main/java/com/example/bookHaven/service/INotificationService.java
@@ -1,4 +1,22 @@
 package com.example.bookHaven.service;
 
+import com.example.bookHaven.entity.Reader;
+import com.example.bookHaven.entity.Notification;
+
+import java.util.List;
+
 public interface INotificationService {
+    Notification create(Notification Notification);
+    Notification update(Notification Notification);
+    Notification findById(String id);
+    List<Notification> findByReader(String ReaderId);
+    List<Notification> findByReader(Reader Reader);
+    boolean existsById(String id);
+    boolean existsByReader(String ReaderId);
+    boolean existsByReader(Reader Reader);
+    boolean deleteById(String id);
+    boolean deleteByReader(String ReaderId);
+    boolean deleteByReader(Reader Reader);
+    List<Notification> listAll();
+    int count();
 }
