3223fa2b8b8d69ec85f690054f09b452949eff52
Tiziano Bruno <brntiziano96@gmail.com>
2024-11-21 22:42:15 +0100

    spring security removal

--- backend/src/test/java/com/example/bookHaven/controller/ReaderControllerTest.java
+++ backend/src/test/java/com/example/bookHaven/controller/ReaderControllerTest.java
@@ -1,15 +1,12 @@
 package com.example.bookHaven.controller;
 
-import com.example.bookHaven.configuration.TestSecurityConfig;
 import com.example.bookHaven.service.implementation.ReaderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(ReaderController.class)
-@Import(TestSecurityConfig.class)
 public class ReaderControllerTest {
 
     @Autowired
