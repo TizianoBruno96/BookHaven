43266704872107f34c1306194991027005955b10
Tiziano Bruno <brntiziano96@gmail.com>
2024-12-09 13:45:48 +0100

    refactoring

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,26 +1,21 @@
-import React, {useEffect, useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './BookList.css';
 
 const BookList = () => {
     const [error, setError] = useState(null);
     const [books, setBooks] = useState([]);
-    const [sortConfig, setSortConfig] = useState({key: '', direction: 'asc'});
     const [currentPage, setCurrentPage] = useState(1);
-    const booksPerPage = 24;
 
+    const booksPerPage = 24;
     const navigate = useNavigate();
 
     const fetchBooks = async () => {
         try {
             const response = await fetch('http://localhost:8080/book/listAll');
-
-            if (!response.ok) {
-                throw new Error("Error in fetching books API");
-            }
+            if (!response.ok) throw new Error('Error in fetching books API');
 
             const data = await response.json();
-
             setBooks(data);
             setError(null);
         } catch (err) {
@@ -29,49 +24,29 @@ const BookList = () => {
         }
     };
 
-    const handleSort = (columnKey) => {
-        let direction = 'asc';
-        if (sortConfig.key === columnKey && sortConfig.direction === 'asc') {
-            direction = 'desc';
-        }
+    const formatDate = (date) => {
+        const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+        return new Date(date).toLocaleDateString('it-IT', options);
+    };
 
-        const sortedBooks = [...books].sort((a, b) => {
-            if (a[columnKey] < b[columnKey]) return direction === 'asc' ? -1 : 1;
-            if (a[columnKey] > b[columnKey]) return direction === 'asc' ? 1 : -1;
-            return 0;
-        });
+    const handleNextPage = () => {
+        if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+    };
 
-        setBooks(sortedBooks);
-        setSortConfig({key: columnKey, direction: direction});
+    const handlePrevPage = () => {
+        if (currentPage > 1) setCurrentPage(currentPage - 1);
     };
 
     const handleBookClick = (bookId) => {
         navigate(`/book/${bookId}`);
     };
 
-    const handleNextPage = () => {
-        if (currentPage < totalPages) {
-            setCurrentPage(currentPage + 1);
-        }
-    };
-
-    const handlePrevPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
-    };
+    const totalPages = Math.ceil(books.length / booksPerPage);
+    const currentBooks = books.slice((currentPage - 1) * booksPerPage, currentPage * booksPerPage);
 
     useEffect(() => {
         fetchBooks();
     }, []);
-
-    const formatDate = (date) => {
-        const options = {day: '2-digit', month: '2-digit', year: 'numeric'};
-        return new Date(date).toLocaleDateString('it-IT', options);
-    };
-
-    const totalPages = Math.ceil(books.length / booksPerPage);
-    const currentBooks = books.slice((currentPage - 1) * booksPerPage, currentPage * booksPerPage);
 
     return (
         <div className="container">
